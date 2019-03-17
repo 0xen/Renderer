ff47b0350c2fcc48f8d0db6a295e7c8915876907
John Green <jkgreen@uclan.ac.uk>
2019-03-17 00:41:13 +0000

    Fixed some bugs *patch*

--- renderer/include/renderer/IModelPool.hpp
+++ renderer/include/renderer/IModelPool.hpp
@@ -17,9 +17,11 @@ namespace Renderer
 		virtual ~IModelPool() {};
 		bool Indexed();
 		virtual IModel * CreateModel() = 0;
+		virtual IModel* GetModel(int index) = 0;
 		virtual void RemoveModel(IModel* model) = 0;
 		virtual void Update() = 0;
 		virtual void AttachBuffer(unsigned int index, IUniformBuffer * buffer) = 0;
+		virtual void UpdateModelBuffer(unsigned int index) = 0;
 		virtual void AttachDescriptorSet(unsigned int index, IDescriptorSet* descriptor_set) = 0;
 		virtual std::vector<IDescriptorSet*> GetDescriptorSets() = 0;
 		virtual void SetVertexDrawCount(unsigned int count) = 0;
